ddd8860343b28a29d6c92dd944133a29123630fb
Kevin Nielsen <knielsen24@gmail.com>
2022-04-26 13:07:50 -0700

    commit tree c5935235c927a08ef3e936547fe3709256cec770

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // code your solution here
+saturdayFun("bathe my dog");
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+
+const mondayWork = function (workLocation = "go to the office") {
+    return `This Monday, I will ${workLocation}.`;
+}
+
+console.log(mondayWork());
+
+
+
+/*
+
+function wrapAdjective(result = "*") {
+    const msg = "You are";
+    return function(str = "a hard worker") {
+        return `${msg} ${result}${str}${result}!`;
+    }
+}
+*/
+
+function wrapAdjective(result = "*", msg = "You are") { 
+    const innerFunction = function (str = "a hard worker") {
+        return `${msg} ${result}${str}${result}!`;
+    }
+    return innerFunction;
+}
+
+wrapAdjective();
+console.log(wrapAdjective("||")("a dedicated programmer"));
